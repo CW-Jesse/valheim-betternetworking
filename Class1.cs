54effb3da8fc190b42502496ddd8ac8f23ad729c
CW Jesse <jesse@confusedwizard.com>
2021-10-11 20:41:34 -0700

    Groundwork

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace Mod_ImprovedNetworking
+{
+    [BepInPlugin("Valheim.CW_Jesse.ImprovedNetworking", "Improved Networking", "0.0.1")]
+    [BepInProcess("valheim.exe")]
+    public class ImprovedNetworking : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("Valheim.CW_Jesse.ImprovedNetworking");
+
+        void Awake() {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(ZDOMan, ZDOMan.)]
+    }
+}
