be66bd9e2eb75ddbc2296e423c950012350f7e72
CW Jesse <jesse@confusedwizard.com>
2021-10-13 10:50:45 -0700

    Discrete options for network update rate

--- BetterNetworking.cs
+++ BetterNetworking.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 using K4os.Compression.LZ4;
 using System.Reflection;
+using System.ComponentModel;
 
 namespace Mod_ImprovedNetworking {
 
@@ -12,24 +13,55 @@ namespace Mod_ImprovedNetworking {
     [BepInProcess("valheim.exe")]
     public class ImprovedNetworking : BaseUnityPlugin {
 
-        static private ConfigEntry<float> configNetworkUpdateRate;
+        static private ConfigEntry<Options_NetworkUpdateRates> configNetworkUpdateRate;
+        private enum Options_NetworkUpdateRates {
+            [Description("100% (20 updates/second)")]
+            _100,
+            [Description("80% (16 updates/second)")]
+            _80,
+            [Description("60% (12 updates/second)")]
+            _60,
+            [Description("40% (8 updates/second)")]
+            _40,
+            [Description("20% (4 updates/second)")]
+            _20
+        }
 
         private readonly Harmony harmony = new Harmony("CW_Jesse.BetterNetworking");
 
         void Awake() {
             harmony.PatchAll();
 
-            configNetworkUpdateRate = Config.Bind("Networking",
-                                                  "NetworkUpdateRateMultiplier",
-                                                  1.0f,
-                                                  "(Default: 1.0) Allows you to increase or decrease how often your Valheim checks to see if it can update peers. As of Patch 0.203.11, 1.0 is 20 times/second. 0.5 is 10 times/second.");
+            configNetworkUpdateRate = Config.Bind(
+                "Networking",
+                "NetworkUpdateRate",
+                Options_NetworkUpdateRates._100,
+                new ConfigDescription(
+                    "(Default: 1.0) Allows you to reduce network strain by reducing the number of updates your computer sends out. As of Patch 0.203.11: 1.0=20 updates/second, 0.1=2 updates/second"
+                )
+            );
         }
 
         [HarmonyPatch(typeof(ZDOMan), "SendZDOToPeers")]
         class NetworkUpdateFrequency_Patch {
 
             static void Prefix(ref float dt) {
-                dt = dt * configNetworkUpdateRate.Value;
+                float networkUpdateRate = 1.0f;
+                switch (configNetworkUpdateRate.Value) {
+                    case Options_NetworkUpdateRates._80:
+                        networkUpdateRate = 0.8f;
+                        break;
+                    case Options_NetworkUpdateRates._60:
+                        networkUpdateRate = 0.6f;
+                        break;
+                    case Options_NetworkUpdateRates._40:
+                        networkUpdateRate = 0.4f;
+                        break;
+                    case Options_NetworkUpdateRates._20:
+                        networkUpdateRate = 0.2f;
+                        break;
+                }
+                dt = dt * networkUpdateRate;
             }
         }
     }
