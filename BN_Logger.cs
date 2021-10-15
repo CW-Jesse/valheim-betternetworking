4fc57a1ce62cb1f28e9afa02a3aff2e0adc3dea1
CW Jesse <jesse@confusedwizard.com>
2021-10-14 20:11:33 -0700

    option to increase/decrease maximum send rate option to decrease minimum send rate log messages option to prevent logging info messages

---
+++ BN_Logger.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Jesse.BetterNetworking {
+    class BN_Logger : BaseUnityPlugin {
+        public static ManualLogSource logger;
+
+        public static void LogError(object data) {
+            BN_Logger.logger.LogError(data);
+        }
+        public static void LogWarning(object data) {
+            BN_Logger.logger.LogWarning(data);
+        }
+        public static void LogMessage(object data) {
+            BN_Logger.logger.LogMessage(data);
+        }
+        public static void LogInfo(object data) {
+            if (BetterNetworking.configLogMessages.Value == true) {
+                BN_Logger.logger.LogInfo(data);
+            }
+        }
+
+    }
+}
--- BetterNetworking.cs
+++ BetterNetworking.cs
@@ -2,18 +2,24 @@
 using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
+using Steamworks;
 
 using K4os.Compression.LZ4;
 using System.Reflection;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System;
+using BepInEx.Logging;
 
-namespace Mod_ImprovedNetworking {
+namespace CW_Jesse.BetterNetworking {
 
-    [BepInPlugin("CW_Jesse.BetterNetworking", "Better Networking", "0.0.2")]
+    [BepInPlugin("CW_Jesse.BetterNetworking", "Better Networking", "0.3.1")]
     [BepInProcess("valheim.exe")]
-    public class ImprovedNetworking : BaseUnityPlugin {
+    public class BetterNetworking : BaseUnityPlugin {
 
-        static private ConfigEntry<Options_NetworkUpdateRates> configNetworkUpdateRate;
+        public static ConfigEntry<bool> configLogMessages;
+
+        private static ConfigEntry<Options_NetworkUpdateRates> configNetworkUpdateRate;
         private enum Options_NetworkUpdateRates {
             [Description("100% (20 updates/second)")]
             _100,
@@ -27,19 +33,104 @@ namespace Mod_ImprovedNetworking {
             _20
         }
 
+        private static ConfigEntry<Options_NetworkSendRateMin> configNetworkSendRateMin;
+        private enum Options_NetworkSendRateMin {
+            [Description("100% (128 KB/s | 1 Mbit/s))")]
+            _100,
+            [Description("50% (64 KB/s | 0.5 Mbit/s))")]
+            _50
+        }
+
+        private static ConfigEntry<Options_NetworkSendRateMax> configNetworkSendRateMax;
+        private enum Options_NetworkSendRateMax {
+            [Description("400% (512 KB/s | 4 Mbit/s)")]
+            _400,
+            [Description("200% (256 KB/s | 2 Mbit/s)")]
+            _200,
+            [Description("100% (128 KB/s | 1 Mbit/s)")]
+            _100,
+            [Description("50% (64 KB/s | 0.5 Mbit/s)")]
+            _50
+        }
+
         private readonly Harmony harmony = new Harmony("CW_Jesse.BetterNetworking");
 
         void Awake() {
-            harmony.PatchAll();
+            BN_Logger.logger = base.Logger;
+
+            configLogMessages = Config.Bind(
+                "Logging",
+                "Log Info Messages",
+                false,
+                "True: Verbose logs.\nFalse: Only log warnings and errors."
+                );
 
             configNetworkUpdateRate = Config.Bind(
                 "Networking",
-                "NetworkUpdateRate",
+                "Update Rate",
                 Options_NetworkUpdateRates._100,
                 new ConfigDescription(
-                    "You can reduce network strain by reducing the number of updates your computer sends out. Values are correct as of patch 0.203.11."
+                    "You can reduce network strain by reducing the number of updates your computer sends out. Displayed values are correct as of patch 0.203.11."
                 )
             );
+
+            configNetworkSendRateMin = Config.Bind(
+                "Networking",
+                "Minimum Send Rate",
+                Options_NetworkSendRateMin._100,
+                new ConfigDescription(
+                    "Steam attempts to estimate your bandwidth. Valheim sets the MINIMUM estimation at 128 KB/s as of patch 0.203.11."
+                )
+            );
+
+            configNetworkSendRateMax = Config.Bind(
+                "Networking",
+                "Maximum Send Rate",
+                Options_NetworkSendRateMax._100,
+                new ConfigDescription(
+                    "Steam attempts to estimate your bandwidth. Valheim sets the MAXIMUM estimation at 128 KB/s as of patch 0.203.11."
+                )
+            );
+
+            harmony.PatchAll();
+
+            configNetworkSendRateSettings_Listen();
+            }
+
+        private static bool configNetworkSendRates_listening = false;
+        public static void configNetworkSendRateSettings_Listen() {
+            if (!configNetworkSendRates_listening) {
+                configNetworkSendRates_listening = true;
+                configNetworkSendRateMin.SettingChanged += ConfigNetworkSendRateMin_SettingChanged;
+                configNetworkSendRateMax.SettingChanged += ConfigNetworkSendRateMax_SettingChanged;
+                BN_Logger.LogInfo("Started listening for user changes to NetworkSendRates");
+            }
+        }
+        public static void configNetworkSendRateSettings_Unlisten() {
+            if (configNetworkSendRates_listening) {
+                configNetworkSendRates_listening = false;
+                configNetworkSendRateMin.SettingChanged -= ConfigNetworkSendRateMin_SettingChanged;
+                configNetworkSendRateMax.SettingChanged -= ConfigNetworkSendRateMax_SettingChanged;
+                BN_Logger.LogInfo("Stopped listening for user changes to NetworkSendRates");
+            }
+        }
+
+        private static void ConfigNetworkSendRateMin_SettingChanged(object sender, EventArgs e) {
+            if (configNetworkSendRateMin.Value == Options_NetworkSendRateMin._100 &&
+                configNetworkSendRateMax.Value == Options_NetworkSendRateMax._50) {
+                configNetworkSendRateMax.Value = Options_NetworkSendRateMax._100;
+            }
+            configNetworkSendRateSettings_Unlisten();
+            NetworkSendRate_Patch.SetSendRateMinFromConfig();
+            configNetworkSendRateSettings_Listen();
+        }
+        private static void ConfigNetworkSendRateMax_SettingChanged(object sender, EventArgs e) {
+            if (configNetworkSendRateMax.Value == Options_NetworkSendRateMax._50) {
+                configNetworkSendRateMin.Value = Options_NetworkSendRateMin._50;
+            }
+            configNetworkSendRateSettings_Unlisten();
+            NetworkSendRate_Patch.SetSendRateMaxFromConfig();
+            configNetworkSendRateSettings_Listen();
         }
 
         [HarmonyPatch(typeof(ZDOMan), "SendZDOToPeers")]
@@ -61,8 +152,117 @@ namespace Mod_ImprovedNetworking {
                         networkUpdateRate = 0.2f;
                         break;
                 }
-                dt = dt * networkUpdateRate;
+                dt *= networkUpdateRate;
             }
         }
+
+        [HarmonyPatch(typeof(SteamNetworkingUtils), nameof(SteamNetworkingUtils.SetConfigValue))]
+        class NetworkSendRate_Patch {
+            static private int originalNetworkSendRateMin = 0;
+            static private bool originalNetworkSendRateMin_set = false;
+            static private int originalNetworkSendRateMax = 0;
+            static private bool originalNetworkSendRateMax_set = false;
+
+
+            public static int sendRateMin {
+                get {
+                    switch (configNetworkSendRateMin.Value) {
+                        case Options_NetworkSendRateMin._50:
+                            return originalNetworkSendRateMin / 2;
+                    }
+                    return originalNetworkSendRateMin;
+                }
+            }
+            public static int sendRateMax {
+                get {
+                    switch (configNetworkSendRateMax.Value) {
+                        case Options_NetworkSendRateMax._400:
+                            return originalNetworkSendRateMax * 4;
+                        case Options_NetworkSendRateMax._200:
+                            return originalNetworkSendRateMax * 2;
+                        case Options_NetworkSendRateMax._50:
+                            return originalNetworkSendRateMax / 2;
+                    }
+                    return originalNetworkSendRateMax;
+                }
+            }
+
+            public static void SetSendRateMinFromConfig() {
+                if (!originalNetworkSendRateMin_set) {
+                    BN_Logger.LogWarning("Attempted to set NetworkSendRateMin before Valheim did");
+                    return;
+                }
+
+                BN_Logger.LogMessage($"Setting NetworkSendRateMin to {sendRateMin}");
+                SetSteamNetworkConfig(ESteamNetworkingConfigValue.k_ESteamNetworkingConfig_SendRateMin, sendRateMin);
+            }
+            public static void SetSendRateMaxFromConfig() {
+                if (!originalNetworkSendRateMax_set) {
+                    BN_Logger.LogWarning("Attempted to set NetworkSendRateMax before Valheim did");
+                    return;
+                }
+
+                BN_Logger.LogMessage($"Setting NetworkSendRateMax to {sendRateMax}");
+                SetSteamNetworkConfig(ESteamNetworkingConfigValue.k_ESteamNetworkingConfig_SendRateMax, sendRateMax);
+            }
+
+            private static void SetSteamNetworkConfig(ESteamNetworkingConfigValue valueType, int value) {
+                GCHandle pinned_SendRateMax = GCHandle.Alloc(value, GCHandleType.Pinned);
+                SteamNetworkingUtils.SetConfigValue(
+                    valueType,
+                    ESteamNetworkingConfigScope.k_ESteamNetworkingConfig_Global,
+                    IntPtr.Zero,
+                    ESteamNetworkingConfigDataType.k_ESteamNetworkingConfig_Int32,
+                    pinned_SendRateMax.AddrOfPinnedObject()
+                    );
+                pinned_SendRateMax.Free();
+            }
+
+            static void Prefix(
+                ESteamNetworkingConfigValue eValue,
+                ESteamNetworkingConfigScope eScopeType,
+                IntPtr scopeObj,
+                ESteamNetworkingConfigDataType eDataType,
+                ref IntPtr pArg) {
+
+                if (eScopeType == ESteamNetworkingConfigScope.k_ESteamNetworkingConfig_Global &&
+                    scopeObj == IntPtr.Zero &&
+                    eDataType == ESteamNetworkingConfigDataType.k_ESteamNetworkingConfig_Int32) {
+
+                    switch (eValue) {
+                        case ESteamNetworkingConfigValue.k_ESteamNetworkingConfig_SendRateMin:
+                            if (!originalNetworkSendRateMin_set) {
+                                originalNetworkSendRateMin_set = true;
+                                originalNetworkSendRateMin = Marshal.ReadInt32(pArg);
+
+                                BN_Logger.LogMessage($"Valheim's default NetworkSendRateMin is {originalNetworkSendRateMin}");
+                            }
+                            break;
+                        case ESteamNetworkingConfigValue.k_ESteamNetworkingConfig_SendRateMax:
+                            if (!originalNetworkSendRateMax_set) {
+                                originalNetworkSendRateMax_set = true;
+                                originalNetworkSendRateMax = Marshal.ReadInt32(pArg);
+                                BN_Logger.LogMessage($"Valheim's default NetworkSendRateMax is {originalNetworkSendRateMin}");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ZSteamSocket), "RegisterGlobalCallbacks")]
+        class PreventValheimControlOfNetworkRate_Patch {
+
+            static void Postfix() {
+                BN_Logger.LogInfo("Network settings overwritten by Valheim; setting them to Better Networking values");
+                BetterNetworking.configNetworkSendRateSettings_Unlisten();
+
+                NetworkSendRate_Patch.SetSendRateMinFromConfig();
+                NetworkSendRate_Patch.SetSendRateMaxFromConfig();
+
+                BetterNetworking.configNetworkSendRateSettings_Listen();
+            }
+        }
+
     }
 }
