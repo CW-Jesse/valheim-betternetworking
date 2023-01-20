5d6a2036f64617fc9a7ed6a2af0eb7822e1b290b
CW Jesse <jesse@confusedwizard.com>
2023-01-20 12:49:03 -0800

    allow forcing crossplay disabled project version: 2.0.2

--- CW_Jesse.BetterNetworking/Patches/BN_Patch_ForceCrossplay.cs
+++ CW_Jesse.BetterNetworking/Patches/BN_Patch_ForceCrossplay.cs
@@ -8,28 +8,33 @@ namespace CW_Jesse.BetterNetworking {
     [HarmonyPatch]
     public class BN_Patch_ForceCrossplay {
         public enum Options_ForceCrossplay {
-            [Description("Enabled <b>[default]</b>")]
-            @true,
-            [Description("Disabled")]
-            @false
+            [Description("Crossplay ENABLED <b>[default]</b>")]
+            playfab,
+            [Description("Crossplay DISABLED")]
+            steamworks,
+            [Description("Vanilla behaviour")]
+            vanilla
         }
 
         public static void InitConfig(ConfigFile config) {
             BetterNetworking.configForceCrossplay = config.Bind(
                 "Networking",
                 "Force Crossplay",
-                Options_ForceCrossplay.@true,
+                Options_ForceCrossplay.playfab,
                 new ConfigDescription(
-                    "Forces dedicated servers to use new PlayFab networking stack. Requires restart."
+                    "Requires restart./nCrossplay enabled: Forces dedicated servers to use new PlayFab networking stack./nCrossplay disabled: Forces dedicated servers to use Steamworks network stack./nVanilla behaviour: Listen for -crossplay flag."
                 ));
         }
 
         [HarmonyPatch(typeof(FejdStartup), "ParseServerArguments")]
         [HarmonyPostfix]
         private static void ForceCrossplay() {
-            if (BetterNetworking.configForceCrossplay.Value == Options_ForceCrossplay.@true) {
+            if (BetterNetworking.configForceCrossplay.Value == Options_ForceCrossplay.playfab) {
                 ZNet.m_onlineBackend = OnlineBackendType.PlayFab;
                 ZPlayFabMatchmaking.LookupPublicIP();
+            }
+            if (BetterNetworking.configForceCrossplay.Value == Options_ForceCrossplay.steamworks) {
+                ZNet.m_onlineBackend = OnlineBackendType.Steamworks;
             }
         }
     }
