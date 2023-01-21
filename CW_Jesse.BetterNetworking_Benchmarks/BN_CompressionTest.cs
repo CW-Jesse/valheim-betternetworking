b2be0e4a86eda6d7b52f33769a4a4ba23f4e8063
CW Jesse <jesse@confusedwizard.com>
2023-01-20 22:31:15 -0800

    added ZstdSharp port added commented code that allows for debugging

--- CW_Jesse.BetterNetworking_Benchmarks/BN_CompressionTest.cs
+++ CW_Jesse.BetterNetworking_Benchmarks/BN_CompressionTest.cs
@@ -8,7 +8,6 @@ using BenchmarkDotNet.Attributes;
 
 using K4os.Compression.LZ4;
 using Ionic.Zlib;
-using ZstdNet;
 
 namespace CW_Jesse.BetterNetworking {
 
@@ -17,23 +16,25 @@ namespace CW_Jesse.BetterNetworking {
 
         byte[] bufferToCompress;
 
-        Compressor zstdCompressor;
-        Compressor zstdCompressorDict0;
-        Compressor zstdCompressorDict1;
-        Decompressor zstdDecompressor;
-        Decompressor zstdDecompressorDict0;
-        Decompressor zstdDecompressorDict1;
+        ZstdNet.Compressor zstdCompressor;
+        ZstdNet.Compressor zstdCompressorDict0;
+        ZstdNet.Compressor zstdCompressorDict1;
+        ZstdNet.Decompressor zstdDecompressor;
+        ZstdNet.Decompressor zstdDecompressorDict0;
+        ZstdNet.Decompressor zstdDecompressorDict1;
 
-        CompressionStream zstdCompressionStream;
+        ZstdSharp.Compressor zstdSharpCompressor;
+
+        ZstdNet.CompressionStream zstdCompressionStream;
 
         byte[] zstdCompressedBuffer;
         byte[] zstdCompressedBufferDict0;
         byte[] zstdCompressedBufferDict1;
 
         //[Params(1)]
-        public CompressionOptions levelZstd = new CompressionOptions(1); // compression level of 1 is fastest and compresses more than 0
-        public CompressionOptions levelZstdDict0;
-        public CompressionOptions levelZstdDict1;
+        public ZstdNet.CompressionOptions levelZstd = new ZstdNet.CompressionOptions(1); // compression level of 1 is fastest and compresses more than 0
+        public ZstdNet.CompressionOptions levelZstdDict0;
+        public ZstdNet.CompressionOptions levelZstdDict1;
 
         public LZ4Level levelLz4 = LZ4Level.L00_FAST; // Better Networking v1.2.2
         public CompressionLevel levelZlib = CompressionLevel.BestCompression; // Valheim default
@@ -42,15 +43,17 @@ namespace CW_Jesse.BetterNetworking {
         public void GlobalSetup() {
             bufferToCompress = File.ReadAllBytes("TestFile");
 
-            byte[] zstdDict0 = DictBuilder.TrainFromBuffer(new[] { bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress });
+            byte[] zstdDict0 = ZstdNet.DictBuilder.TrainFromBuffer(new[] { bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress });
             byte[] zstdDict1 = File.ReadAllBytes("dict.dict"); // a poorly trained dictionary is worse than no dictionary at all
 
-            zstdCompressor = new Compressor(levelZstd);
-            zstdCompressorDict0 = new Compressor(new CompressionOptions(zstdDict0, levelZstd.CompressionLevel));
-            zstdCompressorDict1 = new Compressor(new CompressionOptions(zstdDict1, levelZstd.CompressionLevel));
-            zstdDecompressor = new Decompressor();
-            zstdDecompressorDict0 = new Decompressor(new DecompressionOptions(zstdDict0));
-            zstdDecompressorDict1 = new Decompressor(new DecompressionOptions(zstdDict1));
+            zstdCompressor = new ZstdNet.Compressor(levelZstd);
+            zstdCompressorDict0 = new ZstdNet.Compressor(new ZstdNet.CompressionOptions(zstdDict0, levelZstd.CompressionLevel));
+            zstdCompressorDict1 = new ZstdNet.Compressor(new ZstdNet.CompressionOptions(zstdDict1, levelZstd.CompressionLevel));
+            zstdDecompressor = new ZstdNet.Decompressor();
+            zstdDecompressorDict0 = new ZstdNet.Decompressor(new ZstdNet.DecompressionOptions(zstdDict0));
+            zstdDecompressorDict1 = new ZstdNet.Decompressor(new ZstdNet.DecompressionOptions(zstdDict1));
+
+            zstdSharpCompressor = new ZstdSharp.Compressor(1);
 
             zstdCompressedBuffer = zstdCompressor.Wrap(bufferToCompress);
             zstdCompressedBufferDict0 = zstdCompressorDict0.Wrap(bufferToCompress);
@@ -68,15 +71,16 @@ namespace CW_Jesse.BetterNetworking {
         }
 
         [Benchmark] public void CompressBN_New() => zstdCompressor.Wrap(bufferToCompress);
+        [Benchmark] public void CompressBN_NewPort() => zstdSharpCompressor.Wrap(bufferToCompress).ToArray();
         [Benchmark] public void CompressBN_NewDict0() => zstdCompressorDict0.Wrap(bufferToCompress);
         [Benchmark] public void CompressBN_NewDict1() => zstdCompressorDict1.Wrap(bufferToCompress);
         [Benchmark] public void DecompressBN_New() => zstdDecompressor.Unwrap(zstdCompressedBuffer);
         [Benchmark] public void DecompressBN_NewDict0() => zstdDecompressorDict0.Unwrap(zstdCompressedBufferDict0);
         [Benchmark] public void DecompressBN_NewDict1() => zstdDecompressorDict1.Unwrap(zstdCompressedBufferDict1);
-        [Benchmark] public void TrainDictBN_New() => DictBuilder.TrainFromBuffer(new[] { bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress });
+        [Benchmark] public void TrainDictBN_New() => ZstdNet.DictBuilder.TrainFromBuffer(new[] { bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress, bufferToCompress });
         [Benchmark]
         public void CompressBN_NewStream() {
-            using (zstdCompressionStream = new CompressionStream(new MemoryStream())) {
+            using (zstdCompressionStream = new ZstdNet.CompressionStream(new MemoryStream())) {
                 zstdCompressionStream.Write(bufferToCompress, 0, bufferToCompress.Length);
             }
         }
--- CW_Jesse.BetterNetworking_Benchmarks/Program.cs
+++ CW_Jesse.BetterNetworking_Benchmarks/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using CW_Jesse.BetterNetworking;
 
 namespace CW_Jesse.BetterNetworking_Benchmarks {
     class Program {
         static void Main(string[] args) {
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig()); // uncomment if you want to debug this
             BenchmarkRunner.Run<BN_CompressionTest>();
         }
     }
